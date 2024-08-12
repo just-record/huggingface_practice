166ca8d2b0cd2c63aa79517a3db2019f9298659e
jbsh <opmorn@gmail.com>
2024-08-12 13:15:40 +0900

    feat: Add code for argmax calculation in transformers_autoclass_automodelfor.py

--- transformers/18.transformers_autoclass_automodelfor.py
+++ transformers/18.transformers_autoclass_automodelfor.py
@@ -22,3 +22,6 @@ print(f'pt_predictions: {pt_predictions}')
 # pt_predictions: 
 # tensor([[0.0021, 0.0018, 0.0115, 0.2121, 0.7725],
 #         [0.2084, 0.1826, 0.1969, 0.1755, 0.2365]], grad_fn=<SoftmaxBackward0>)
+
+argmax = pt_predictions.argmax(dim=1)
+print(f'argmax: {argmax}')
