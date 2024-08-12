46d69f2a94d70f395419ee1c754a565eb78f3610
jbsh <opmorn@gmail.com>
2024-08-12 12:47:06 +0900

    chore: Update .gitignore to include flagged/ directory and *.flac files
    This commit updates the .gitignore file to include the flagged/ directory and *.flac files. The flagged/ directory was previously missing from the .gitignore, and the addition ensures that any files within that directory are ignored by Git. Additionally, the inclusion of *.flac files in the .gitignore prevents them from being tracked by Git.
    
    These changes improve the repository by properly ignoring the flagged/ directory and *.flac files, ensuring that they are not accidentally committed or pushed to the repository.

--- transformers/14.transformers_autoclass_autobackbone.py
+++ transformers/14.transformers_autoclass_autobackbone.py
@@ -14,3 +14,8 @@ outputs = model(**inputs)
 feature_maps = outputs.feature_maps
 
 print(feature_maps[0].shape)
+# torch.Size([1, 96, 56, 56])
+# 첫 번째 차원 (1): 배치 크기
+# 두 번째 차원 (96): 채널 수 또는 특성 맵(feature map)의 수
+# 세 번째 차원 (56): 높이
+# 네 번째 차원 (56): 너비
--- transformers/15.transformers_autoclass_autofeatureextractor.py
+++ transformers/15.transformers_autoclass_autofeatureextractor.py
@@ -1,7 +1,22 @@
 from transformers import AutoFeatureExtractor
+import librosa
+import requests
 
-feature_extractor = AutoFeatureExtractor.from_pretrained(
-    "ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition"
-)
+feature_extractor = AutoFeatureExtractor.from_pretrained("ehcalabres/wav2vec2-lg-xlsr-en-speech-emotion-recognition")
 
-print(feature_extractor)
+# 오디오 파일 URL
+audio_url = "https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac"
+
+# 오디오 파일 다운로드 및 로드
+audio_file = requests.get(audio_url).content
+with open("temp_audio.flac", "wb") as f:
+    f.write(audio_file)
+
+# librosa를 사용하여 오디오 로드
+audio, sample_rate = librosa.load("temp_audio.flac", sr=16000)
+
+# 올바른 입력 형식으로 변환
+inputs = feature_extractor(audio, sampling_rate=sample_rate, return_tensors="pt", padding=True)
+print(f'inputs: {inputs}')
+# inputs: {'input_values': tensor([[ 0.0717,  0.0463, -0.0508,  ..., -0.0046,  0.0326, -0.1615]]), 'attention_mask': tensor([[1, 1, 1,  ..., 1, 1, 1]], dtype=torch.int32)}
+
